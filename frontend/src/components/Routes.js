0c7c4faa41e2f0c46548de0c3551e4c8c920c8a8
Ian Walston <ikwalston@comcast.net>
2021-07-21 17:00:11 -0400

    set token at begining of session if token is in storage

--- frontend/src/components/Routes.js
+++ frontend/src/components/Routes.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginForm from "./LoginForm";
 import { Switch, Route } from "react-router-dom";
 import SignUp from "./SignUpForm";
 import Profile from "./Profile";
+import { useUser } from "../hooks";
 
 export default function Routes() {
+
+  const { setToken } = useUser()
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setToken(localStorage.getItem("token"))
+    }
+  }, [])
+
   return (
     <Switch>
       <Route path="/login">
