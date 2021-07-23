92cdec45aea831a14f143a84c2302552b90ac827
Alexa Matei <alexa.matei.dev@icloud.com>
2021-07-23 17:13:50 +0300

    admin editbio

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -26,6 +26,29 @@ router.post('/ban', async (req, res, next) => {
     user.save();
 
     return res.json({ success: true, ban: user.ban })
+});
+
+router.patch('/editbio', async (req, res, next) => {
+    if(!req.headers.authorization) return res.sendStatus(401);
+
+    let token = utils.validateJWT(next, req.headers.authorization);
+    let admUser = await User.findOne({ _id: token.sub });
+    if(!admUser) return res.json({ success: false, msg: "who are you?" });
+
+    if(!admUser.isAdmin) return res.json({ success: false, msg: "you aren't an admin, are you?" });
+
+    if(!req.body.user)
+        return res.json({ success: false, msg: "no user id has been provided." });
+    if(!req.body.newbio)
+        return res.json({ success: false, msg: "no newbio could be found" });
+
+    let user = await User.findOne({ _id: req.body.user });
+    if(!user) return res.json({ success: false, msg: "no user could be found" });
+
+    user.bio = req.body.newbio;
+
+    user.save();
+    return res.json({ success: true, bio: user.bio });
 })
 
 module.exports = router;
