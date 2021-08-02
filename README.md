98b8c475b44fce03fb6a6a8bd35485c9100284bb
Ian Walston <ikwalston@comcast.net>
2021-08-02 19:55:36 -0400

    ui for other user view

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Postman Link
 https://www.getpostman.com/collections/c9fd66f599f168765473
+
+
+remove all aboutyou users in mongo: `db.users.remove({})`
--- frontend/src/components/OtherUser.js
+++ frontend/src/components/OtherUser.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useUser } from "../hooks";
 import { useParams } from "react-router";
+import { Typography, Box, Grid } from "@material-ui/core";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+
+import Link from "./Link";
 
 export default function OtherUser() {
   const { getUser } = useUser();
@@ -29,7 +33,57 @@ export default function OtherUser() {
     <div>
       {errorMessage && <h1>{errorMessage}</h1>}
 
-      {userData && JSON.stringify(userData)}
+      {userData && <><Grid container>
+        <Grid item>
+          <Box
+            alignContent={"center"}
+            bgcolor="white"
+            py={2}
+            px={4}
+            borderRadius={16}
+          >
+            <Typography variant="h4">
+              <AccountCircleIcon color="primary" fontSize={"large"} />
+              {userData.username}
+            </Typography>
+            <Box>
+
+              <Typography gutterBottom component="span">
+                {userData.bio}
+              </Typography>
+            </Box>
+            <Typography variant="body2">email: {userData.email}</Typography>
+            <Typography variant="body2">
+              {" "}
+              joined {new Date(userData.createdAt).toLocaleDateString()}
+            </Typography>
+            <Typography variant="body2">
+              {" "}
+              admin: {userData.isAdmin ? "Yes" : "No"}
+            </Typography>
+            <Typography variant="body2">
+              {" "}
+              verified: {userData.verified.is ? "Yes" : "No"}
+            </Typography>
+          </Box>
+        </Grid>
+      </Grid>
+                 <Grid container justifyContent="center">
+                 <Grid item>
+                   <Box alignContent={"center"} bgcolor="white" borderRadius={16} p={1}>
+                     <Typography variant="caption">links</Typography>
+                     <Box py={1} px={3}>
+                       {userData.links.map((link) => (
+                         <div key={link._id}>
+                           <Link link={link} />
+                         </div>
+                       ))}
+                     </Box>
+                   </Box>
+                 </Grid>
+               </Grid>
+   </>}
+
     </div>
   );
 }
