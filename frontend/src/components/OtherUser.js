ed870d34be3eaeff9d9a331723ee2d09172bef56
Ian Walston <ikwalston@comcast.net>
2021-08-02 16:52:05 -0400

    lint

--- frontend/src/components/OtherUser.js
+++ frontend/src/components/OtherUser.js
@@ -1,42 +1,35 @@
-import React, { useState, useEffect } from 'react'
-import { useUser } from '../hooks'
-import { useParams } from 'react-router';
-
+import React, { useState, useEffect } from "react";
+import { useUser } from "../hooks";
+import { useParams } from "react-router";
 
 export default function OtherUser() {
-    const { getUser } = useUser()
-    const { username } = useParams()
+  const { getUser } = useUser();
+  const { username } = useParams();
 
-    const [errorMessage, setErrorMessage] = useState()
-    const [userData, setUserData] = useState()
+  const [errorMessage, setErrorMessage] = useState();
+  const [userData, setUserData] = useState();
 
-    useEffect(() => {
-        console.log(username)
-        getUser(username).then(res => {
-            console.log(username)
-            console.log(res)
+  useEffect(() => {
+    console.log(username);
+    getUser(username).then((res) => {
+      console.log(username);
+      console.log(res);
 
-            if (!res.data.success && res.data.msg) {
-                setErrorMessage(res.data.msg)
-            }
+      if (!res.data.success && res.data.msg) {
+        setErrorMessage(res.data.msg);
+      }
 
-            if (res.data.success){
-                setUserData(res.data.user)
-            }
-        })
-    }, []);
+      if (res.data.success) {
+        setUserData(res.data.user);
+      }
+    });
+  }, []);
 
-    return (
-        <div>
-            {errorMessage && <h1>
-                {errorMessage}
-            </h1>}
+  return (
+    <div>
+      {errorMessage && <h1>{errorMessage}</h1>}
 
-            {
-                userData && 
-                JSON.stringify(userData)
-            }
-
-        </div>
-    )
+      {userData && JSON.stringify(userData)}
+    </div>
+  );
 }
